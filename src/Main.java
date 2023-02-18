9d8b9ba31d87cd4d40f7b30646b827556f1ab87c
Jerekon <124033477+jerekon@users.noreply.github.com>
2023-02-18 15:14:40 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println ("Hello world!");
+        SalesManager salesManager = new SalesManager(new int[]{11, 21, 48, 34, 14, 1, 42, 8});
+        System.out.println();
+        System.out.println("\"Sales analysis\"");
+        System.out.printf("In this list  %s   " + "max:%n%s у.е.%n", Arrays.toString(salesManager.sales), salesManager.max());
     }
 }
