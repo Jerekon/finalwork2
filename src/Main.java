02d1d757c79a932ced8edb1768cb6399a5ec5a40
Alex Rybtsov <alexander.rybtsov@gmail.com>
2023-03-02 13:19:39 +0300

    switch to long

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
+
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        SalesManager salesManager = new SalesManager(new int[]{11, 21, 48, 34, 14, 1, 42, 8});
+        SalesManager salesManager = new SalesManager(new long[]{11, 21, 48, 34, 14, 1, 42,8});
         System.out.println();
-        System.out.println("\"Sales analysis\"");
-        System.out.printf("In this list  %s   " + "max:%n%s у.е.%n", Arrays.toString(salesManager.sales), salesManager.max());
+        System.out.println("\"Analysis of sales statistics\"");
+        System.out.printf("In the specified list %s we get the maximum amount of sales : %s у.е.%n", Arrays.toString(salesManager.sales), salesManager.max());
+        System.out.println("Average calculation: " + salesManager.cropAverage() + " у.е.");
     }
 }
